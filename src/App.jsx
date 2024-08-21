8dc8ae561dbcc205823f90a7137efac3318e8b11
SastreMax <sastremaximiliano@gmail.com>
2024-08-21 14:09:42 -0300

    Update App.jsx "mostrando el listado" y usando la API
    paso 3: Lista las películas y muestra el título, año y poster.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 function App() {  
   // Definir el estado para almacenar el término de búsqueda
   const [consulta, setConsulta] = useState('');
+  const [peliculas, setPeliculas] = useState([]);
+
+  const API_KEY = '1e8ca98c';
 
   // Función para manejar el evento de cambio en el input
   const manejarCambio = (e) => {
@@ -11,10 +14,24 @@ function App() {
   };
 
   // funcion para manejar el envio del formulario
-  const buscarPeliculas = (e) => {
+  const buscarPeliculas = async (e) => {
     e.preventDefault(); // Prevenir el comportamiento por defecto del formulario 
                         // para evitar que la pagina se recargue
-    console.log(`Buscando peliculas con el termino: ${consulta}`);
+    if (consulta.trim() === '') return;  // Verifica que el input no esté vacío
+  
+    const URL = `http://www.omdbapi.com/?s=${consulta}&apikey=${API_KEY}`;
+  
+    try {
+      const respuesta = await fetch(URL);
+      const datos = await respuesta.json();
+      if (datos.Search) {
+        setPeliculas(datos.Search);   // Actualiza el estado con los resultados
+      } else {
+        setPeliculas([]);  // Limpia los resultados si no hay coincidencias
+      }
+    } catch (error) {
+      console.error('Error al buscar una pelicula:', error);
+    }
   };
 
   return (    
@@ -32,7 +49,19 @@ function App() {
         </form>        
       </header>
       <main>
-        aqui irá el resultado
+        {peliculas.length > 0 ? (
+          <ul>
+            {peliculas.map((pelicula) => (
+              <li key={pelicula.imdbID}>
+                <h2>{pelicula.Title}</h2>
+                <p>{pelicula.Year}</p>
+                <img src={pelicula.Poster} alt={pelicula.Title} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No se encontraron resultados.</p>
+        )}
       </main>
     </div>    
   );
