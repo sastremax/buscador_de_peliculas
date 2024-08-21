86c4febad69be8398d27a7c7c386fe9b780bad2f
SastreMax <sastremaximiliano@gmail.com>
2024-08-21 16:12:15 -0300

    json
    . Se transcriben extensiones json de un objeto de la API para tener una busqueda con resultados y sin resultados

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import withResults from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
+import "./App.css";
 
-function App() {  
+function App() {
   // Definir el estado para almacenar el término de búsqueda
-  const [consulta, setConsulta] = useState('');
+  const [consulta, setConsulta] = useState("");
   const [peliculas, setPeliculas] = useState([]);
-  const [ultimaConsulta, setUltimaConsulta] = useState('');  // Almacena la última búsqueda
+  const [ultimaConsulta, setUltimaConsulta] = useState(""); // Almacena la última búsqueda
 
-  const API_KEY = '1e8ca98c';
+  const API_KEY = "1e8ca98c";
 
   // Función para manejar el evento de cambio en el input
   const manejarCambio = (e) => {
-    setConsulta(e.target.value);  // Actualizar el estado con el valor del input
+    setConsulta(e.target.value); // Actualizar el estado con el valor del input
   };
 
   useEffect(() => {
-    if (consulta.trim() !== '' && consulta !== ultimaConsulta) {   // Solo realiza la búsqueda si el término ha cambiado y no está vacío
-      const buscarPeliculas = async () => {   // funcion para manejar el envio del formulario
+    if (consulta.trim() !== "" && consulta !== ultimaConsulta) {
+      // Solo realiza la búsqueda si el término ha cambiado y no está vacío
+      const buscarPeliculas = async () => {
+        // funcion para manejar el envio del formulario
         const URL = `http://www.omdbapi.com/?s=${consulta}&apikey=${API_KEY}`;
-      
+
         try {
           const respuesta = await fetch(URL);
           const datos = await respuesta.json();
           if (datos.Search) {
-            setPeliculas(datos.Search);   // Actualiza el estado con los resultados
+            setPeliculas(datos.Search); // Actualiza el estado con los resultados
           } else {
-            setPeliculas([]);  // Limpia los resultados si no hay coincidencias
+            setPeliculas([]); // Limpia los resultados si no hay coincidencias
           }
-          setUltimaConsulta(consulta);  // Actualiza la última búsqueda
+          setUltimaConsulta(consulta); // Actualiza la última búsqueda
         } catch (error) {
-          console.error('Error al buscar una pelicula:', error);
+          console.error("Error al buscar una pelicula:", error);
         }
       };
       buscarPeliculas();
     }
-  }, [consulta, ultimaConsulta, API_KEY]);  // Ejecutar cada vez que cambie la consulta
+  }, [consulta, ultimaConsulta, API_KEY]); // Ejecutar cada vez que cambie la consulta
 
-  return (    
+  return (
     <div>
       <header>
         <h1>Buscador de Peliculas</h1>
-        <form className='form' onSubmit={(e) => e.preventDefault()}>
+        <form className="form" onSubmit={(e) => e.preventDefault()}>
           <input
-            type='text'
-            placeholder='Escribe el nombre de la pelicula...'
-            value={consulta}   // El valor del input o campo de texto está ligado al estado "consulta"
-            onChange={manejarCambio}  // Manejar los cambios en el input
+            type="text"
+            placeholder="Escribe el nombre de la pelicula..."
+            value={consulta} // El valor del input o campo de texto está ligado al estado "consulta"
+            onChange={manejarCambio} // Manejar los cambios en el input
           />
-        <button type='submit'>Buscar</button>
-        </form>        
+          <button type="submit">Buscar</button>
+        </form>
       </header>
       <main>
         {peliculas.length > 0 ? (
@@ -65,8 +69,8 @@ function App() {
           <p>No se encontraron resultados.</p>
         )}
       </main>
-    </div>    
+    </div>
   );
 }
 
-export default App
+export default App;
