e1eded7c49678e6f0ea795b3a74693eab0d33709
SastreMax <sastremaximiliano@gmail.com>
2024-08-21 13:19:02 -0300

    App.jsx
    . Segundo paso: Necesita mostrar un input para buscar la película y un botón para buscar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, {useState} from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
+  // Definir el estado para almacenar el término de búsqueda
+  const [consulta, setConsulta] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  // Función para manejar el evento de cambio en el input
+  const manejarCambio = (e) => {
+    setConsulta(e.target.value);  // Actualizar el estado con el valor del input
+  };
+
+  return (    
+    <div>
+      <header>
+        <h1>Buscador de Peliculas</h1>
+        <form className='form'>
+          <input
+            type='text'
+            placeholder='Escribe el nombre de la pelicula...'
+            value={consulta}   // El valor del input o campo de texto está ligado al estado "consulta"
+            onChange={manejarCambio}  // Manejar los cambios en el input
+          />
+        <button type='submit'>Buscar</button>
+        </form>        
+      </header>
+      <main>
+        aqui irá el resultado
+      </main>
+    </div>    
+  );
 }
 
 export default App
